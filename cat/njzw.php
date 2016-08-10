690e5582645286887be21914032f4338b60cbe2a
Goldsir <709872640@qq.com>
2016-08-10 18:05:44 +0800

    2016.8.10.18.05

---
+++ cat/njzw.php
@@ -0,0 +1,14 @@
+<?php
+    $ch = curl_init();
+    $url = 'http://apis.baidu.com/txapi/naowan/naowan';
+    $header = array(
+        'apikey:d44aa60bc3d624a18a205d4513b75090',
+    );
+    // 添加apikey到header
+    curl_setopt($ch, CURLOPT_HTTPHEADER  , $header);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // 执行HTTP请求
+    curl_setopt($ch , CURLOPT_URL , $url);
+    $res = curl_exec($ch);
+	echo $res
+?>
